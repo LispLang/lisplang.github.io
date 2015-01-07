f31d6385013006c512798e4e3567aa244eab1421
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-07 21:09:42 -0200

    Update wiki style and fix javascript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,9 +2,12 @@ function fixCodeClasses(block) {
     /* Jekyll emits <code> tags that have classes like 'language-lisp' or
        'language-markdown'. highlight-lisp allegedly supports those. This isn't
        the case. So this function removes the 'language-' prefix. */
-    if($(block).attr('class').match('language-lisp')) {
-        const new_class = $(block).attr('class').replace('language-', '');
-        $(block).attr('class', new_class);
+    const current_class = $(block).attr('class')
+    if(current_class) {
+        if(current_class.match('language-lisp')) {
+            const new_class = current_class.replace('language-', '');
+            $(block).attr('class', new_class);
+        }
     }
 }
 
@@ -13,14 +16,14 @@ function highlightEverything(block) {
     HighlightLisp.highlight_element(block);
 }
 
-
 $(document).ready(function() {
     /* Create a table of contents */
     $('#toc').toc({
         listType: 'ol',
         noBackToTopLinks: true,
-        headers: 'article h1, article h2, article h3',
-        showSpeed: 0,
+        headers: 'h1, h2, h3',
+        minimumHeaders: 1,
+        showSpeed: 0
     });
     /* Syntax highlighting */
     $('pre code').each(function(i, block) {
