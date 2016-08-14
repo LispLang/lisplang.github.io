a19d34dc0a49f2cce834011379443973c3d870b2
Fernando Borretti <eudoxiahp@gmail.com>
2016-08-14 11:57:22 -0300

    Add wiki.js

---
+++ assets/js/wiki.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  /* Add anchors to headers */
+  const sel = 'article h1, article h2, article h3, article h4, article h5, article h6';
+  $(sel).each(function(index, item) {
+    const id = $(item).attr('id');
+    if (id) {
+      const anchor = '<a href="#' + id + '" class="link"/>';
+      $(item).prepend(anchor);
+    }
+  });
+
+  /* Enable link-hiding */
+  $('#hide-links').on('click', function() {
+    const selector = 'article'
+    const state = $(selector).attr('hide-links');
+    if (state) {
+      $(selector).attr('hide-links', null);
+      $(this).attr('hide-links', null);
+      $(this).text('off');
+    } else {
+      $(selector).attr('hide-links', 'true');
+      $(this).attr('hide-links', 'true');
+      $(this).text('on');
+    }
+  });
+});
